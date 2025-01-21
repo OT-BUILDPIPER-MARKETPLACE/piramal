409022205c95bc1e28febd7f2c12df680c111487
Deepak Gupta <75674594+deepakgupta97@users.noreply.github.com>
2025-01-21 13:01:46 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ function createEntrypointDockerFileJava()
           /usr/bin/mongocryptd &
           java  -javaagent:/app/opentelemetry-1.0.1.jar -Dotel.resource.attributes=service.name=${CODEBASE_DIR} \${JAVA_OPTS} -Dotel.propagators=b3multi -Dotel.traces.exporter=zipkin  -Dotel.exporter.zipkin.endpoint=http://otel-collector.devopsnow.svc.cluster.local:9411  -XX:+UseContainerSupport -XX:MaxRAMPercentage=75.0 -jar /app/service.jar " > entrypoint.sh
    
-    echo "FROM nexus.piramalfinance.com:8082/repository/piramal-docker/java-mongocryptd:java17
+    echo "FROM nexus.piramalfinance.com/repository/piramal-docker/java-mongocryptd:java17
       WORKDIR /app
       COPY entrypoint.sh /app/
       COPY target/*.jar /app/service.jar
@@ -26,7 +26,7 @@ function createEntrypointDockerFileJava()
   else
     echo "#!/bin/bash
           java  -javaagent:/app/opentelemetry-1.0.1.jar -Dotel.resource.attributes=service.name=${CODEBASE_DIR} \${JAVA_OPTS} -Dotel.propagators=b3multi -Dotel.traces.exporter=zipkin  -Dotel.exporter.zipkin.endpoint=http://otel-collector.devopsnow.svc.cluster.local:9411  -XX:+UseContainerSupport -XX:MaxRAMPercentage=75.0 -jar /app/service.jar " > entrypoint.sh
-          echo "FROM nexus.piramalfinance.com:8082/repository/piramal-docker/prf-java:java17
+          echo "FROM nexus.piramalfinance.com/repository/piramal-docker/prf-java:java17
                   WORKDIR /app
                   COPY target/*.jar /app/service.jar
                   ADD https://nexus.piramalfinance.com/repository/piramal-releases/com/pchf/opentelemetry/1.24.0/opentelemetry-1.24.0.jar /app
@@ -45,14 +45,14 @@ function createEntrypointDockerFileNode()
     local service_name=$1
     if [[ $service_name == "apf-web" || $service_name == "partner-central-web" || $service_name == "insurance-web" || $service_name == "incentive-management-web" || $service_name == "direct-assignment-web" || $service_name == "prospect-web" || $service_name == "pchfweb" || $service_name == "esign-mf-web" || $service_name == "review-mfe-web" || $service_name == "customer-satisfaction-survey-web" || $service_name == "pq-portal-web" || $service_name == "ai-web" || $service_name == "chatbot-web" || $service_name == "khata-web" || $service_name == "platform-tools-web" || $service_name == "shakti-web" || $service_name == "telesales-web" || $service_name == "customer-account-web" || $service_name == "credit-report-web" || $service_name == "communication-template-management-web" || $service_name == "offer-central-web" || $service_name == "tele-collection-web" || $service_name == "credit-central-web" || $service_name == "sx-engagement-mf-web" || $service_name == "tatkal-web" || $service_name == "kyc-web" || $service_name == "policy-designer-web" || $service_name == "policy-manager-web" || $service_name == "url-redirect-web" || $service_name == "pd-platform-web" || $service_name == "sc-customer-web" || $service_name == "vendor-management-web" || $service_name == "collections-web" || $service_name == "login-mfe-web" || $service_name == "bank-verification-mf-web" ]];
     then
-            echo "FROM nexus.piramalfinance.com:8082/repository/piramal-docker/node-18.17.0-alpine
+            echo "FROM nexus.piramalfinance.com/repository/piramal-docker/node-18.17.0-alpine
             WORKDIR /app
             COPY ./ /app/
             ENV NODE_OPTIONS=\"--max-old-space-size=4096\"
             RUN yarn install --network-timeout 1000000 \
             && yarn build"> Dockerfile
     else
-            echo "FROM nexus.piramalfinance.com:8082/repository/piramal-docker/node-18.14.0-alpine
+            echo "FROM nexus.piramalfinance.com/repository/piramal-docker/node-18.14.0-alpine
             WORKDIR /app
             COPY ./ /app/
             ENV NODE_OPTIONS=\"--max-old-space-size=4096\"
